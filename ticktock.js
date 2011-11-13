cf225789e36a70a5b08f2165d1160d5e5fd2fc7d
Clinton Montague <clinton@slightlymore.co.uk>
2011-11-13 19:46:46 +0000

    Tidied up javascript and wrapped it in a closure because that's cool

--- ticktock.js
+++ ticktock.js
@@ -1,69 +1,68 @@
-var	background = document.body.style,
-	clock,
-	seconds,
-	minutes,
-	hours;
+(function () {
+	var	clock,
+		seconds,
+		minutes,
+		hours;
 
-if (typeof Array.prototype.map !== "function") {
-	Array.prototype.map = function (callback) {
-		var r = [],
-			length = this.length;
+	if (typeof Array.prototype.map !== "function") {
+		Array.prototype.map = function (callback) {
+			var r = [],
+				length = this.length;
 
-		for (var i = 0; i < length; i++) {
-			r.push(callback(this[i]));
-		}
-		return r;
-	};
-}
-
-
-function $ (id) {
-	return document.getElementById(id);
-}
-
-function init () {
-	clock = $("clock");
-	seconds = $("seconds");
-	minutes = $("minutes");
-	hours = $("hours");
-
-	document.body.removeChild ($("message"));
-
-	tick ();
-	setInterval (tick, 500);
-}
-
-function normalise (scalar, maxValue, divisions) {
-	return parseFloat(scalar, 10) * maxValue / divisions;
-}
-
-function getBackgroundColor (h, m, s) {
-	h = normalise (h, 255, 24);
-	m = normalise (m, 255, 60);
-	s = normalise (s, 255, 60);
-	var time = [h, m, s].map (stringify);
-	return "rgb(" + time.join(",") + ")";
-}
-
-function stringify (number) {
-	number = ~~number;
-	if (number < 10) {
-		number = "0" + number;
+			for (var i = 0; i < length; i++) {
+				r.push(callback(this[i]));
+			}
+			return r;
+		};
 	}
-	return number;
-}
 
-function tick () {
-	var time = new Date(),
-		s = time.getSeconds (),
-		m = time.getMinutes (),
-		h = time.getHours ();
-	hours.innerHTML = stringify(h);
-	minutes.innerHTML = stringify(m);
-	seconds.innerHTML = stringify(s);
+	function $ (id) {
+		return document.getElementById(id);
+	}
 
-	document.body.style.backgroundColor = getBackgroundColor (h, m, s);
-}
+	function normalise (scalar, maxValue, divisions) {
+		return parseFloat(scalar, 10) * maxValue / divisions;
+	}
 
+	function stringify (number) {
+		number = ~~number;
+		if (number < 10) {
+			number = "0" + number;
+		}
+		return number;
+	}
 
-init ();
+	function getBackgroundColor (h, m, s) {
+		h = normalise (h, 255, 24);
+		m = normalise (m, 255, 60);
+		s = normalise (s, 255, 60);
+		var time = [h, m, s].map (stringify);
+		return "rgb(" + time.join(",") + ")";
+	}
+
+	function tick () {
+		var time = new Date(),
+			s = time.getSeconds (),
+			m = time.getMinutes (),
+			h = time.getHours ();
+		hours.innerHTML = stringify(h);
+		minutes.innerHTML = stringify(m);
+		seconds.innerHTML = stringify(s);
+
+		document.body.style.backgroundColor = getBackgroundColor (h, m, s);
+	}
+
+	function init () {
+		clock = $("clock");
+		seconds = $("seconds");
+		minutes = $("minutes");
+		hours = $("hours");
+
+		document.body.removeChild ($("message"));
+
+		tick ();
+		setInterval (tick, 200);
+	}
+
+	init ();
+})();
