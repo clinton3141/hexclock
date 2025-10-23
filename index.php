ce98ba96a261128d8cb3aff8d4efa539a1735a79
Clinton Montague <clinton3141@pm.me>
2025-10-23 16:46:19 +0100

    chore: remove more social and tracking nonsense

--- index.php
+++ index.php
@@ -86,19 +86,6 @@ function hextime ()
 		<link type="text/css" rel="stylesheet" href="?style" />
 		<link rel="apple-touch-icon" href="touch-icon.png"/>
 		<link rel="shortcut icon" href="/favicon.ico" />
-		<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-2513896-18']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-
-		</script>
 	</head>
 	<body>
 		<h1>Hex Clock</h1>
@@ -108,10 +95,7 @@ function hextime ()
 
 
 		<aside>
-			<div class="fb-like" data-href="http://hexclock.slightlymore.co.uk/" data-send="false" data-layout="button_count" data-width="112" data-show-faces="true"></div>
-			<a href="https://twitter.com/share" class="twitter-share-button" data-count="none" data-via="iblamefish">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>	<iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.html?screen_name=iblamefish&show_count=false" style="width:150px; height:20px;"></iframe>
-			<a href="http://github.com/iblamefish/hexclock"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
-			<div class="g-plusone" data-size="medium"></div>
+			<a href="http://github.com/iblamefish/hexclock">Github</a>
 			<script src="ticktock.js"></script>
 		</aside>
 	</body>
